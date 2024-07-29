06de41dc57cd45bc5e040e4cf9efaeef4bf205e4
PauloBorszcz <paulofelipeborszcz2004@gmail.com>
2024-07-29 14:25:58 -0300

    Adicionando um Blog para a Aplicação e toda a integração com o GhostCMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
+import starlightImageZoom from 'starlight-image-zoom'
+import starlightGhostCMS from '@matthiesenxyz/starlight-ghostcms';
+import starlightHeadingBadges from 'starlight-heading-badges'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
       title: "MMHub",
+      plugins: [starlightImageZoom(), starlightGhostCMS(
+        {
+          url: 'http://137.184.150.175',
+          key: 'ec5913e856e6d7d13532f167a1'
+        }
+      ), starlightHeadingBadges()],
       components: {
         LastUpdated: './src/components/CustomLastUpdate.astro',
       },
